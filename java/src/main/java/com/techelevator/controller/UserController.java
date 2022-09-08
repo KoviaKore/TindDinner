23ae66fcaac69c20fed1925246ce629954947449
Cassidy LeFevre <lefevrecassidy@gmail.com>
2022-09-07 23:12:09 -0500

    Can get user object by username

--- java/src/main/java/com/techelevator/controller/UserController.java
+++ java/src/main/java/com/techelevator/controller/UserController.java
@@ -5,6 +5,7 @@ import com.techelevator.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +17,10 @@ public class UserController {
     @GetMapping("/user/{id}")
     User getById(@PathVariable long id){
         return userRepository.findByUserId(id);
+    }
+
+    @GetMapping("/find-user")
+    User getByUsername(@RequestParam String username){
+        return userRepository.findByUsername(username);
     }
 }
