ef8250a8327232e75d320f801936d6a06373daec
Cassidy LeFevre <lefevrecassidy@gmail.com>
2022-09-09 08:44:33 -0500

    Crossorigin annotation added to user controller.

--- java/src/main/java/com/techelevator/controller/UserController.java
+++ java/src/main/java/com/techelevator/controller/UserController.java
@@ -3,14 +3,12 @@ package com.techelevator.controller;
 import com.techelevator.Repository.UserRepository;
 import com.techelevator.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 //TODO: Write exception handling for if a user searched does not exist---- maybe maybe not, it already returns nothing.
 //TODO: Use security so user can only view own data, unless admin.
 
+@CrossOrigin
 @RestController
 public class UserController {
 
